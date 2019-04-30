48312a8e59688092f08c74cd2d4105f4ec6cf4c4
abdellah <selassi.abdellah@gmail.com>
2019-04-30 10:14:45 +0200

    Post/Pre increment

---
+++ src/PostPreIncrement.java
@@ -0,0 +1,14 @@
+public class PostPreIncrement {
+    public static void main(String[] args) {
+        int a = 1;
+
+        System.out.println(a++); //You will see 1
+
+        //Now a is 2
+        System.out.println(a); //You will see 1
+
+
+        System.out.println(++a); //You will see 3
+
+    }
+}
